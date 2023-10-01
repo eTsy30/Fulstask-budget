115ba08f5b9d6044e4eb100680f35605680eba08
tsy <tsy@mail.com>
2023-10-01 17:44:33 +0300

    wer

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,20 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
+import serverless = require('serverless-http'); //
+
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors();
   await app.listen(4545);
   console.log('Server started');
+  const expressApp = app.getHttpAdapter().getInstance(); //
+  return serverless(expressApp); //
 }
 bootstrap();
+
+let server;
+export const handler = async (event, context, callback) => {
+  server = server ?? (await bootstrap());
+  return server(event, context, callback);
+};
