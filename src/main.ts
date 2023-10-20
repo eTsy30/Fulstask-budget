1857c56073636e52120ea7ba0edb4bbae40274b1
tsy <tsy@mail.com>
2023-10-21 00:24:37 +0300

    d

--- src/main.ts
+++ src/main.ts
@@ -1,19 +1,20 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import helmet from 'helmet';
-import cors from 'cors';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  // app.enableCors();
+  app.enableCors();
   app.use(helmet());
   app.use(
-    cors({
-      origin: ['https://client-swart-sigma-79.vercel.app'],
-      methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
-      credentials: true, // Если вы используете сессии или аутентификацию
+    helmet.contentSecurityPolicy({
+      directives: {
+        defaultSrc: ["'self'"], // Разрешить только загрузку с текущего источника
+        scriptSrc: ["'self'", 'https://client-budget.vercel.app'], // Разрешить загрузку скриптов только с вашего фронтенда
+        // Другие директивы по необходимости
+      },
     }),
   );
-  await app.listen('https://fulstask-budget.vercel.app' || 4545);
+  await app.listen(4545);
   console.log('Server started');
 }
 bootstrap();
