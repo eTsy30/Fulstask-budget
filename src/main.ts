8bfff2438a4d04ef102b2086cd754a201bfa654a
tsy <tsy@mail.com>
2023-10-21 00:31:18 +0300

    dfgh

--- src/main.ts
+++ src/main.ts
@@ -1,19 +1,20 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import helmet from 'helmet';
+// import helmet from 'helmet';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.enableCors();
-  app.use(helmet());
-  app.use(
-    helmet.contentSecurityPolicy({
-      directives: {
-        defaultSrc: ["'self'"], // Разрешить только загрузку с текущего источника
-        scriptSrc: ["'self'", 'https://client-budget.vercel.app'], // Разрешить загрузку скриптов только с вашего фронтенда
-        // Другие директивы по необходимости
-      },
-    }),
-  );
+  // app.enableCors(false);
+
+  // app.use(helmet());
+  // app.use(
+  //   helmet.contentSecurityPolicy({
+  //     directives: {
+  //       defaultSrc: ["'self'"], // Разрешить только загрузку с текущего источника
+  //       scriptSrc: ["'self'", 'https://client-budget.vercel.app'], // Разрешить загрузку скриптов только с вашего фронтенда
+  //       // Другие директивы по необходимости
+  //     },
+  //   }),
+  // );
   await app.listen('https://fulstask-budget.vercel.app' || 4545);
   console.log('Server started');
 }
