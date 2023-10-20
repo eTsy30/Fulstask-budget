5547f71c51cce43ca1528f8988dbebb3e4dc25d7
tsy <tsy@mail.com>
2023-10-21 00:04:00 +0300

    d

--- src/main.ts
+++ src/main.ts
@@ -1,17 +1,16 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import helmet from 'helmet';
+import cors from 'cors';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors();
   app.use(helmet());
   app.use(
-    helmet.contentSecurityPolicy({
-      directives: {
-        defaultSrc: ["'self'"], // Разрешить только загрузку с текущего источника
-        scriptSrc: ["'self'", 'https://client-budget.vercel.app'], // Разрешить загрузку скриптов только с вашего фронтенда
-        // Другие директивы по необходимости
-      },
+    cors({
+      origin: ['https://client-swart-sigma-79.vercel.app'],
+      methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+      credentials: true, // Если вы используете сессии или аутентификацию
     }),
   );
   await app.listen(4545);
