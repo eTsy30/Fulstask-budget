6103a47751634ced15da7ba015c8cebe18d0894d
tsy <tsy@mail.com>
2023-11-06 16:50:50 +0300

    fix2

--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,22 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors();
 
+  const allowedOrigins = [
+    'http://localhost:5173',
+    'https://fulstask-budget.vercel.app',
+  ];
+
+  app.use((req, res, next) => {
+    const origin = req.headers.origin;
+    if (allowedOrigins.includes(origin)) {
+      res.header('Access-Control-Allow-Origin', origin);
+      res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+      res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+      res.header('Access-Control-Allow-Credentials', true); // Разрешить куки
+    }
+    next();
+  });
+
   await app.listen(4545);
   console.log('Server started');
 }
