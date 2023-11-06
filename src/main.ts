941f0f4195eeaa653fc8d5b55a4a50c60f08e00a
tsy <tsy@mail.com>
2023-11-06 16:35:21 +0300

    fix

--- src/main.ts
+++ src/main.ts
@@ -1,26 +1,10 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-// import helmet from 'helmet';
+
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.use((req, res, next) => {
-    res.header('Access-Control-Allow-Origin', '*');
-    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
-    res.header('Access-Control-Allow-Headers', 'Content-Type, Accept');
-    next();
-  });
-  // app.enableCors(false);
+  app.enableCors();
 
-  // app.use(helmet());
-  // app.use(
-  //   helmet.contentSecurityPolicy({
-  //     directives: {
-  //       defaultSrc: ["'self'"], // Разрешить только загрузку с текущего источника
-  //       scriptSrc: ["'self'", 'https://client-budget.vercel.app'], // Разрешить загрузку скриптов только с вашего фронтенда
-  //       // Другие директивы по необходимости
-  //     },
-  //   }),
-  // );
   await app.listen(4545);
   console.log('Server started');
 }
