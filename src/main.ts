9f50acb5ced9424063ed5aac1082d5c15223f83f
tsy <tsy@mail.com>
2023-11-12 00:51:48 +0300

    as

--- src/main.ts
+++ src/main.ts
@@ -1,27 +1,9 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-
+import cors from 'cors';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-
-  const allowedOrigins = [
-    'http://localhost:5173',
-    'https://client-budget.vercel.app',
-  ];
-
-  app.enableCors({
-    origin: (origin, callback) => {
-      if (allowedOrigins.includes(origin)) {
-        callback(null, true);
-      } else {
-        callback(new Error('Not allowed by CORS'));
-      }
-    },
-    methods: 'GET, POST, PUT, DELETE',
-    allowedHeaders: 'Content-Type, Authorization',
-    credentials: true, // Разрешить куки
-  });
-
+  app.use(cors());
   await app.listen(4545);
   console.log('Server started');
 }
