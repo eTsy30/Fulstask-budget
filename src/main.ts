8bb94d9c3ac7efe8c40d036b2f72e95346ba532e
tsy <tsy@mail.com>
2023-11-12 00:54:52 +0300

    ayy

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,7 @@ import { AppModule } from './app.module';
 import cors from 'cors';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.use(cors());
+  app.use(cors({ origin: ['https://client-budget.vercel.app'] }));
   await app.listen(4545);
   console.log('Server started');
 }
