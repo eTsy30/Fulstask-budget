44d575884cfdf601fb25a42bc3b5b63e19d3d0a8
tsy <tsy@mail.com>
2023-11-17 21:16:27 +0300

    пав

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,15 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import * as cors from 'cors';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.use(cors());
+
+  app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', 'http://localhost:5173');
+    // другие разрешенные заголовки и методы...
+    next();
+  });
+
   await app.listen(4545);
   console.log('Server started');
 }
